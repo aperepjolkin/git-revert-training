eae4665e0627dd57015c69e0a98c6d4b9a4dcd9a
Aleksandrs Perepjolkins <aleksandrs.perepjolkins@intrum.com>
2021-02-15 13:12:30 +0200

    'test;

---
+++ Class.cs
@@ -0,0 +1,22 @@
+namespace Revert
+{
+   public class TestClass {
+    
+    public void TestMethod(){
+        for (int i = 0; i < length; i++)
+        { }
+
+        
+    }
+
+    public void AnotherMethod {
+        do {
+            int test = 1;
+        } 
+        while (test < 10) {
+            test++;
+        }
+    }
+   }
+
+}
